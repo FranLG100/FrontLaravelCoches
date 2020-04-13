4b83d45cbb62952467a06129d779f7372f05b3a0
Fran <fralg100@gmail.com>
2020-04-13 11:53:42 +0200

    Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,23 +1,57 @@
 import {Component, OnInit} from '@angular/core';
 import {Router,ActivatedRoute,Params} from '@angular/router';
+import {User} from '../../models/user';
+import {UserService} from '../../services/user.service';
 
 @Component({
     selector:'login',
-    templateUrl:'./login.component.html'
+    templateUrl:'./login.component.html',
+    providers:[UserService]
 })
 
 export class LoginComponent implements OnInit{
 
     public title:string;
+    public user:User;
+    public token;
+    public identity;
 
     constructor(
         //private _route:ActivatedRoute,
         //private _router:Router
+        private _userService:UserService,
     ){
         this.title='Identifícate';
+        this.user=new User(1,'ROLE_USER','','','','');
     }
 
     ngOnInit(){
         console.log('login.component cargado correctamente');
+        let user=this._userService.getIdentity();
+        console.log(user.name);
+    }
+
+    onSubmit(form){
+        console.log(this.user);
+
+        this._userService.signup(this.user).subscribe(
+            response=>{
+                //Token
+                this.token=response;
+                localStorage.setItem('token',this.token);
+                //Token descodificado
+                this._userService.signup(this.user,true).subscribe(
+                    response=>{
+                        this.identity=response; 
+                        localStorage.setItem('identity',JSON.stringify(this.identity));
+                    },error=>{
+                        console.log(<any>error);
+                    }
+                );
+
+            },error=>{
+                console.log(<any>error);
+            }
+        );
     }
 }
