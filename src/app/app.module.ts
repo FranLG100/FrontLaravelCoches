5636807c4ffb5c33d3d4488d2502b20490b44fee
Fran <fralg100@gmail.com>
2020-04-13 09:56:27 +0200

    Primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { routing, appRoutingProviders } from './app.routing';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
+  ],
+  imports: [
+    BrowserModule,
+    routing,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [
+    appRoutingProviders
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/models/car.ts
@@ -0,0 +1,11 @@
+export class Car{
+    constructor(
+        public id:number,
+        public title:string,
+        public description:string,
+        public price:string,
+        public status:Date,
+        public createdAt:string,
+        public updatedAt:string
+    ){}
+}
